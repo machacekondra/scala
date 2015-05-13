cd557a2a5c9401ec672f0cdf71373962277c9b45
Ondra Machacek <omachace@redhat.com>
2015-05-13 16:51:40 +0200

    final chang roberts

--- src/main/scala/ChangRoberts.scala
+++ src/main/scala/ChangRoberts.scala
@@ -6,21 +6,43 @@ import akka.routing._
  * Created by ondra on 5/13/15.
  */
 object ChangRoberts extends App {
-  val RING_SIZE: Int = 5
+  val RING_SIZE: Int = 2000
 
-  case class LeaderFound(uid: Int)
+  case object ElectionDone
+  case class LeaderFound(puid: Int, uid: Int)
   case class Election(myuid: Int, uid: Int)
 
   class Process(puid: Int) extends Actor {
+    var participant: Boolean = false
+    var leader: Boolean = false
+    var leaderUid: Int = _
+
     def receive = {
       case Election(myuid: Int, uid: Int) =>
-        println(self + " - " + puid + " - " + uid)
+        //println("Election" + self + " - " + puid + " - " + uid)
         if(puid == uid) {
-          sender ! LeaderFound(uid)
+          participant = false
+          leader = true
+          leaderUid = puid
+          sender ! LeaderFound(puid, puid)
         } else if(uid < puid) {
-          sender ! Election(puid, puid)
+          if(!participant) {
+            participant = true
+            sender ! Election(puid, puid)
+          }
         } else {
+          participant = true
           sender ! Election(puid, uid)
+        }
+      case LeaderFound(myuid:Int, uid: Int) =>
+        //println("LeaderFound" + self + " - " + puid + " - " + uid)
+        if(leader) {
+          println("Leader found with id " + leaderUid)
+          sender ! ElectionDone
+        } else {
+          participant = false
+          leaderUid = uid
+          sender ! LeaderFound(puid, uid)
         }
     }
   }
@@ -45,6 +67,8 @@ object ChangRoberts extends App {
           message match {
             case Election(myuid: Int, uid: Int) =>
               List(Destination(sender, routees(nextUid(myuid) - 1)))
+            case LeaderFound(myuid: Int, uid: Int) =>
+              List(Destination(sender, routees(nextUid(myuid) - 1)))
           }
       }
     }
@@ -60,8 +84,9 @@ object ChangRoberts extends App {
     def receive = {
       case Election(myuid: Int, uid: Int) =>
         ringRoute ! Election(myuid, uid)
-      case LeaderFound(uid: Int) =>
-        println("Leader found: " + uid)
+      case LeaderFound(myuid: Int, puid: Int) =>
+        ringRoute ! LeaderFound(myuid, puid)
+      case ElectionDone =>
         //ringRoute ! Broadcast(PoisonPill)
         self ! PoisonPill
         context.system.shutdown()
@@ -71,5 +96,8 @@ object ChangRoberts extends App {
   val system = ActorSystem("ChangRoberts")
   val ring = system.actorOf(Props(new Ring()), name = "ring")
   ring ! Election(1, 1)
+  ring ! Election(132, 132)
+  ring ! Election(1320, 1320)
+  ring ! Election(888, 888)
 
 }
